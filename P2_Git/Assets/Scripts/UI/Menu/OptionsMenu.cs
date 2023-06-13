8de5d969eb1311716fe8fb6186da059d0bbd82e8
Zoey Goldschmidt <zoeygoldschmidt@gmail.com>
2023-06-13 08:34:34 +0200

    ResolutinSelection Implementation

--- P2_Git/Assets/Scripts/UI/Menu/OptionsMenu.cs
+++ P2_Git/Assets/Scripts/UI/Menu/OptionsMenu.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volumeMaster", volume);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
