40bc03c41e790bad7cd0d2c4bc83164f6d8253e9
Hakon <hakon.wind@stud.h-da.de>
2023-06-22 08:48:52 +0200

    tape update // dotween implementation

---
+++ P2_Git/Assets/Scripts/tape/tapematerial.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class tapematerial : MonoBehaviour
+{
+    public TapeSettings tapesettings;
+    public List<GameObject> go;
+    public List<Material> tapemats;
+    private float fillspeed;
+
+    private void Awake()
+    {
+        fillspeed = tapesettings.fillspeed;
+        go = new List<GameObject>();
+        tapemats = new List<Material>();
+
+        foreach (Transform child in this.transform)
+        {
+            go.Add(child.gameObject);
+            tapemats.Add(child.GetComponent<MeshRenderer>().material);
+
+        }
+    }
+    void Start()
+    {
+        foreach (Material mat in tapemats)
+        {
+            mat.DOFloat(1, "_Fillamount", fillspeed);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
