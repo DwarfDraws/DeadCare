1b09a57bcfbfc757dfb6c7c62b7b978a55038cd1
Zoey Goldschmidt <zoeygoldschmidt@gmail.com>
2023-06-23 06:18:17 +0200

    residual files removed

--- P2_Git/Assets/Scripts/AnimationScriptRevision/IdleSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ChildAnimController")]
-public class IdleSO : ScriptableObject
-{
-    public AnimatorOverrideController animatorOV;
-}
