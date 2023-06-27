d650567311b984305013bf7d724e03d5e2e14cb5
doodler345 <lukas.salewsky@gmail.com>
2023-06-27 13:06:23 +0200

    DOTween Implementation

---
+++ P2_Git/Assets/Scripts/UI/DOTweenScale.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class DOTweenScale : MonoBehaviour
+{
+
+    private Vector3 stars_initScale;
+    private Vector3 stars_scaleTo;
+
+    [SerializeField] int starID;
+    static float stars_tweeningFactor;
+    static float stars_tweeningDuration_seconds;
+    static float tweeningOffset_seconds;
+    static float waitTime_atMaxScale_seconds;
+
+    void Start()
+    {
+        stars_initScale = transform.localScale;
+        stars_scaleTo = stars_initScale * stars_tweeningFactor;
+
+        float waitTime = tweeningOffset_seconds * starID;
+        Invoke("StartTweening", waitTime);
+        
+    }
+
+    void StartTweening()
+    {
+        transform.DOScale(stars_scaleTo, stars_tweeningDuration_seconds)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() =>
+            {
+                transform.DOScale(stars_initScale, stars_tweeningDuration_seconds)
+                    .SetEase(Ease.OutBounce)
+                    .SetDelay(waitTime_atMaxScale_seconds)
+                    .OnComplete(StartTweening);
+            });
+    }
+
+
+    public void SetupTweening(float factor, float duration_sec, float offset_sec, float waitTime_atMax_sec) 
+    {
+        stars_tweeningFactor = factor;
+        stars_tweeningDuration_seconds = duration_sec;
+        tweeningOffset_seconds = offset_sec;
+        waitTime_atMaxScale_seconds = waitTime_atMax_sec;
+    }
+
+
+}
