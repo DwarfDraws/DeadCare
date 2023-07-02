7e39bc453a292475699c7cad8f693c6053e94aa9
NastyCat7 <nastjazockt@gmail.com>
2023-07-02 17:53:29 +0200

    color edits for halos and timer
    sick timer color change wowwwwww

--- P2_Git/Assets/Scripts/Widget.cs
+++ P2_Git/Assets/Scripts/Widget.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class Widget : MonoBehaviour
 {
-
+    public Color red;
+    public Color yellow;
+    public Color green;
     Image img;
 
     private void Start() 
@@ -21,5 +23,16 @@ public class Widget : MonoBehaviour
     public void UpdateWidget(float timer)
     {
         img.fillAmount = timer;
+        img.color = green;
+        
+        if (timer <= 0.5f)
+        {
+            img.color = yellow;
+
+            if (timer <= 0.25f)
+            {
+                img.color = red;
+            }
+        }
     }
 }
